14c824290c2d718b9fdd3d205afa717427b7a1c4
Rihen <rihenww@gmail.com>
2026-02-14 10:42:32 +0530

    Time: 2 ms (83.77%), Space: 57.1 MB (28.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,21 @@
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0) return false;
+
+            const top = stack.pop();
+            if (top !== map[char]) return false;
+        }
+    }
+
+    return stack.length === 0;
+};
