1bb6a09122ee8cecbaafcef40ef17159ced6ad18
Rihen <rihenww@gmail.com>
2026-02-14 10:48:56 +0530

    Time: 0 ms (100%), Space: 56.6 MB (82.9%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,25 @@
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode(-1);  // temporary node
+    let current = dummy;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    // Attach remaining nodes
+    if (list1 !== null) {
+        current.next = list1;
+    } else {
+        current.next = list2;
+    }
+
+    return dummy.next;
+};
+
