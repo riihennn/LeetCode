48a1672eaa728634e45afa8f5f2f7bb493281486
Rihen <rihenww@gmail.com>
2026-03-25 15:13:11 +0530

    Time: 4 ms (57.41%), Space: 57.6 MB (22.42%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -0,0 +1,16 @@
+var removeOuterParentheses = function(s) {
+    let result = "";
+    let depth = 0;
+
+    for (let ch of s) {
+        if (ch === "(") {
+            if (depth > 0) result += ch;
+            depth++;
+        } else {
+            depth--;
+            if (depth > 0) result += ch;
+        }
+    }
+
+    return result;
+};
